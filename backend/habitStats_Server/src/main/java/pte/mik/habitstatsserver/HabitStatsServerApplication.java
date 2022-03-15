2b4ff0d7ac63636feefdc8749b6add057fb9956a
PSFHU <prokszamano@gmail.com>
2022-03-15 17:35:49 +0100

    Spring App init - Spring

---
+++ backend/habitStats_Server/src/main/java/pte/mik/habitstatsserver/HabitStatsServerApplication.java
@@ -0,0 +1,13 @@
+package pte.mik.habitstatsserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitStatsServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HabitStatsServerApplication.class, args);
+    }
+
+}
---
+++ backend/habitStats_Server/src/test/java/pte/mik/habitstatsserver/HabitStatsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pte.mik.habitstatsserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitStatsServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
